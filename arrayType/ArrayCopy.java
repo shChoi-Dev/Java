f41a6b99f1a55e515394bb32f1738526e19cfd4b
ChoiSeungHun <sehn47@dgu.ac.kr>
2025-07-29 11:54:34 +0900

    style: 자바 기초 문법 학습 | 250729
    - 오타수정(arrayCopy)

--- arrayType/ArrayCopy.java
+++ arrayType/ArrayCopy.java
@@ -2,7 +2,7 @@ package arrayType;
 
 public class ArrayCopy {
 	public static void main(String[] args) {
-		//앝은 복사 : 참조 주소만 복사(변수들끼리 참조값 공유) =>  = 이용
+		//얕은 복사 : 참조 주소만 복사(변수들끼리 참조값 공유) =>  = 이용
 		//변수 이름만 다르고 동일한 배열 참조
 		//원본 변수를 통해 변경하면 복사본 참조 내용도 변경됨
 		int[] a = {1,2,3,4};
