cf5a8eba667cfc727313fb4f5afe8a355bb70ec9
ChoiSeungHun <sehn47@dgu.ac.kr>
2025-08-01 14:01:43 +0900

    style: 자바 기초 문법 학습 | 250801
    - 코드수정(SmartTelevision)

--- interfacePkg/sec03/SmartTelevision.java
+++ interfacePkg/sec03/SmartTelevision.java
@@ -1,6 +1,6 @@
 package interfacePkg.sec03;
 
-import interfacePkg.sec02.IRemoteControl;
+//import interfacePkg.sec02.IRemoteControl; 코드 복사에 의해 자동 삽입될 수 있음
 
 public class SmartTelevision implements ISearchable, IRemoteControl {
 	private int volume; //인스턴스 멤버필드
